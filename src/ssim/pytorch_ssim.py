5d3d9976d13d8f9400961126932d92b136edb568
NielsPoldervaart <Niels@meghira.nl>
2024-05-11 16:36:41 +0200

    Changed output to show percentage similarity Changed image inputs to use image1 and image2

--- src/ssim/pytorch_ssim.py
+++ src/ssim/pytorch_ssim.py
@@ -14,25 +14,25 @@ transform = v2.Compose(
 )
 
 img1 = (
-    transform(Image.open("./images/image13.jpg").convert("RGB")).unsqueeze(0).to(device)
+    transform(Image.open("./images/image1.jpg").convert("RGB")).unsqueeze(0).to(device)
 )
 img2 = (
-    transform(Image.open("./images/image14.jpg").convert("RGB")).unsqueeze(0).to(device)
+    transform(Image.open("./images/image2.jpg").convert("RGB")).unsqueeze(0).to(device)
 )
 
 # https://pypi.org/project/pytorch-msssim/
 # https://github.com/VainF/pytorch-msssim
 # Using the ssim and ms_ssim functions
 # NOTE: The ssim and ms_ssim don't need to be moved to the GPU, as they use the same device as the input tensors (cuda in this case)
-ssim_val = 1 - ssim(img1, img2, data_range=255, size_average=True)
+ssim_val = ssim(img1, img2, data_range=255, size_average=True)
 print(f"ssim: {ssim_val.item() * 100:.2f}%")
-ms_ssim_val = 1 - ms_ssim(img1, img2, data_range=255, size_average=True)
+ms_ssim_val = ms_ssim(img1, img2, data_range=255, size_average=True)
 print(f"ms_ssim: {ms_ssim_val.item() * 100:.2f}%")
 
 # Using the SSIM and MS_SSIM classes
 ssim_module = SSIM(data_range=255, size_average=True, channel=3).to(device)
-ssim_val = 1 - ssim_module(img1, img2)
+ssim_val = ssim_module(img1, img2)
 print(f"SSIM: {ssim_val.item() * 100:.2f}%")
 ms_ssim_module = MS_SSIM(data_range=255, size_average=True, channel=3).to(device)
-ms_ssim_val = 1 - ms_ssim_module(img1, img2)
+ms_ssim_val = ms_ssim_module(img1, img2)
 print(f"MS_SSIM: {ms_ssim_val.item() * 100:.2f}%")
